95616de4dffa69e816fc08301425b17d07be3c9c
Mykhailo Omelchuk <siteofmichal@gmail.com>
2020-07-17 14:44:56 +0300

    rewrited exceptions to Python 3 form, refactored views

--- django_visual/ide/views.py
+++ django_visual/ide/views.py
@@ -11,13 +11,14 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.core.management.base import CommandError
 from django.conf import settings, Settings
+from django.views.decorators.http import require_http_methods, require_POST
 
-from create_project import (
+from .create_project import (
 	copy_project_template,
 	copy_application_template
 )
 
-from open_project import (
+from .open_project import (
 	project_context,
 	project_settings,
 	edit_installed_apps,
@@ -25,7 +26,7 @@ from open_project import (
 	application_edit_model
 )
 
-from run import run_manage
+from .run import run_manage
 
 
 def index(request):
@@ -54,6 +55,7 @@ def index(request):
 	return render(request, 'index.html', context)
 
 
+@require_http_methods(["POST", "GET"])
 def create_project(request):
 	"""
 	Create new Django project
@@ -75,7 +77,7 @@ def create_project(request):
 
 		try:
 			copy_project_template(template, title)
-		except CommandError, e:
+		except CommandError as e:
 			context['title'] = title
 			context['error'] = str(e)
 			return render(request, 'create_project.html', context)
@@ -98,75 +100,70 @@ def open_project(request, project_id):
 	return render(request, 'open_project.html', context)
 
 
+@require_POST
 def create_application(request, project_id):
 	"""
 	Creates new application for given project
 	"""
 	project_home = join(settings.PROJECTS_HOME, project_id)
 
-	if request.method == "POST":
-		app_name = request.POST.get("app_name")
-		copy_application_template(project_home, app_name)
+	app_name = request.POST.get("app_name")
+	copy_application_template(project_home, app_name)
 
-		pr_settings = project_settings(project_id, project_home)
-		apps = pr_settings.INSTALLED_APPS
-		apps.append(app_name)
+	pr_settings = project_settings(project_id, project_home)
+	apps = pr_settings.INSTALLED_APPS
+	apps.append(app_name)
 
-		edit_installed_apps(project_id, project_home, apps)
+	edit_installed_apps(project_id, project_home, apps)
 
-		return HttpResponse("OK")
-	else:
-		return HttpResponse("POST 'app_name' of new application to create")
+	return HttpResponse("OK")
 
 
+@require_POST
 def add_application(request, project_id):
 	"""
 	Add existing application to INSTALLED_APPS
 	"""
 	project_home = join(settings.PROJECTS_HOME, project_id)
 
-	if request.method == "POST":
-		app_name = request.POST.get("app_name")
-		pr_settings = project_settings(project_id, project_home)
-		apps = pr_settings.INSTALLED_APPS
-		apps.append(app_name)
+	app_name = request.POST.get("app_name")
+	pr_settings = project_settings(project_id, project_home)
+	apps = pr_settings.INSTALLED_APPS
+	apps.append(app_name)
 
-		edit_installed_apps(project_id, project_home, apps)
+	edit_installed_apps(project_id, project_home, apps)
 
-		return HttpResponse("OK")
-	else:
-		return HttpResponse("POST 'app_name' of new application to add")
+	return HttpResponse("OK")
 
 
+@require_POST
 def remove_application(request, project_id):
 	"""
 	Remove existing application from INSTALLED_APPS
 	"""
 	project_home = join(settings.PROJECTS_HOME, project_id)
 
-	if request.method == "POST":
-		app_name = request.POST.get("app_name")
-		pr_settings = project_settings(project_id, project_home)
-		apps = pr_settings.INSTALLED_APPS
-		apps.remove(app_name)
+	app_name = request.POST.get("app_name")
+	pr_settings = project_settings(project_id, project_home)
+	apps = pr_settings.INSTALLED_APPS
+	apps.remove(app_name)
 
-		edit_installed_apps(project_id, project_home, apps)
+	edit_installed_apps(project_id, project_home, apps)
 
-		return HttpResponse("OK")
-	else:
-		return HttpResponse("POST 'app_name' of new application to remove")
+	return HttpResponse("OK")
 
 
+@require_POST
 def add_model(request, project_id):
 	"""
 	Creates new model in application specified in POST data
 	"""
 	project_home = join(settings.PROJECTS_HOME, project_id)
 
-	if request.method == "POST":
-		application_add_model(project_id, project_home, request.POST)
+	application_add_model(project_id, project_home, request.POST)
 
 	return redirect("open_project", project_id=project_id)
+
 
 def open_file(request):
 	"""
@@ -174,6 +171,7 @@ def open_file(request):
 	"""
 
 	path = request.GET.get("path", "")
+	print(path)
 
 	if not path:
 		return HttpResponse("")
@@ -184,21 +182,19 @@ def open_file(request):
 	return HttpResponse(content, content_type='application/octet-stream')
 
 
+@require_POST
 def save_file(request):
 	"""
 	Saves file in IDE editor.
 	"""
 
-	if request.method == "POST":
-		path = request.POST.get("path", "")
-		content = request.POST.get("content", "")
+	path = request.POST.get("path", "")
+	content = request.POST.get("content", "")
 
-		with open(path, 'w') as f:
-			f.write(content)
+	with open(path, 'w') as f:
+		f.write(content)
 
-		return HttpResponse("File saved")
-
-	return HttpResponse("POST 'path' and 'content' of file to save")
+	return HttpResponse("File saved")
 
 
 def run_project(request, project_id):
@@ -218,18 +214,16 @@ def run_project(request, project_id):
 		return HttpResponse(data)
 
 
+@require_POST
 def stop_project(request, project_id):
 	"""
 	Kills running python with manage.py inside for project
 	"""
 
-	if request.method == "POST":
-		pid = request.POST.get("pid", "")
-		if pid:
-			try:
-				os.kill(int(pid), 9)
-				return HttpResponse("OK")
-			except OSError, e:
-				return HttpResponse(str(e))
-
-	return HttpResponse("")
+	pid = request.POST.get("pid", "")
+	if pid:
+		try:
+			os.kill(int(pid), 9)
+			return HttpResponse("Ok")
+		except OSError as e:
+			return HttpResponse(str(e))
